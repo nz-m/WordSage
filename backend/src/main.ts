f797d1d568ce40ad67d55714638ce6aea61e1a72
Neaz Mahmud <neaz6160@gmail.com>
2023-08-28 12:39:45 +0600

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,27 +3,36 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
+  // Configuration for Swagger documentation
+  const swaggerConfig = new DocumentBuilder()
     .setTitle('WordSage API')
     .setDescription('The WordSage API description')
     .setVersion('1.0')
     .addTag('wordsage')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
 
-  // Increase the payload size limit
-  app.use(express.json({ limit: '10mb' }));
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
+  // Configuration for payload size limit
+  const payloadSizeLimit = '10mb';
+
+  // Apply middleware before starting the app
+  app.use(express.json({ limit: payloadSizeLimit }));
 
   // Apply global validation pipe
   app.useGlobalPipes(new ValidationPipe());
 
+  // Enable Cross-Origin Resource Sharing (CORS)
   app.enableCors();
 
-  await app.listen(4000);
+  const PORT = 4000;
+  await app.listen(PORT);
+  console.log(`Application is running on port ${PORT}`);
 }
 
 bootstrap();
